07bab53e9cee6f2f0ca70eb1d1416ae8226fdc22
PorterOBrien2021 <824085@mystma.org>
2021-02-04 15:37:36 +0000

    letter

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#WRITE YOUR CODE IN THIS FILE
+def letterPos(w, x):
+    for i in range(0, len(w)):
+        if i == "x":
+            return w[x]
+        else:
+            return 0
+
+print(letterPos("reed", "e"))
